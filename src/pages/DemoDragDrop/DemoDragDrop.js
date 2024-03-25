70f18e43e54f770bb80cacb8c2abba71c1ae49cd
BaoNguyen2301 <138358439+BaoNguyen2301@users.noreply.github.com>
2024-03-26 01:21:12 +0700

    Edit Task P11

--- src/pages/DemoDragDrop/DemoDragDrop.js
+++ src/pages/DemoDragDrop/DemoDragDrop.js
@@ -1,6 +1,8 @@
-import React, { useState } from 'react'
+import React, { useRef, useState } from 'react'
+import { useSpring, animated } from '@react-spring/web'
+import './DemoDragDrop.css'
 
-const taskList = [
+const defaultValue = [
     { id: 1, taskName: 'Task 1' },
     { id: 2, taskName: 'Task 2' },
     { id: 3, taskName: 'Task 3' },
@@ -10,20 +12,60 @@ const taskList = [
 
 export default function DemoDragDrop(props) {
 
-    // const [taskList, setTaskList] = useState();
+    const [taskList, setTaskList] = useState(defaultValue);
+    const tagDrag = useRef({})
+    const tagDragEnter = useRef({})
+
+    //Animation
+    const [propsSpring, set, stop] = useSpring(
+        () => ({
+            from: { bottom: -25 },
+            to: { bottom: 0 },
+            config: { duration: 250 },
+            reset: true
+        }),
+        []
+    )
 
     const handleDragStart = (e, task, index) => {
-        console.log('tag', e.target);
-        console.log('task', task);
-        console.log('index', index);
+        // console.log('tag', e.target);
+        // console.log('task', task);
+        // console.log('index', index);
+        tagDrag.current = task;
     }
 
-    const handleDragOver = (e) => {
-        console.log('tagover', e.target);
+    const handleDragOver = (e, taskDragEnter, index) => {
+        // console.log('tagover', e.target);
+
+    }
+
+    const handleDragEnter = (e, taskDragEnter, index) => {
+
+        set({ bottom: 0 })
+        // console.log('tagend', e.target);
+
+        console.log(taskDragEnter)
+
+        tagDragEnter.current = { ...taskDragEnter }
+
+        let taskListUpdate = [...taskList]
+        //Lay ra index thang dang keo
+        let indexDragTag = taskListUpdate.findIndex(task => task.id === tagDrag.current.id)
+        //Lay ra index thang bi keo qua
+        let indexDragEnter = taskListUpdate.findIndex(task => task.id === taskDragEnter.id)
+
+        let temp = taskListUpdate[indexDragTag]
+
+        taskListUpdate[indexDragTag] = taskListUpdate[indexDragEnter]
+
+        taskListUpdate[indexDragEnter] = temp
+
+        setTaskList(taskListUpdate)
     }
 
     const handleDragEnd = (e) => {
-        console.log('tagend', e.target);
+
+
     }
 
     const handleDrop = (e) => {
@@ -32,18 +74,45 @@ export default function DemoDragDrop(props) {
     }
 
     return (
-        <div className='container'>
+        <div className='container' onDragOver={(e) => {
+            e.stopPropagation();
+            e.preventDefault();
+        }} onDrop={(e) => {
+            tagDrag.current = {};
+
+            setTaskList([...taskList])
+        }}>
             <div className='text-center display-4' onDragOver={handleDragOver}> Task list</div>
             <div className='row'>
                 <div className='col-4'></div>
                 <div className='bg-dark p-5 col-4'>
                     {taskList.map((task, index) => {
+
+                        let cssDragTag = task.id === tagDrag.current.id ? 'dragTag' : '';
+
+                        if (task.id === tagDragEnter.current.id) {
+                            return <animated.div
+                                style={{
+                                    position: 'relative',
+                                    bottom: propsSpring.bottom.to([0, 1], ['-25px', '0px']),
+                                }}
+                                draggable='true'
+                                onDragStart={(e) => { handleDragStart(e, task, index) }}
+                                onDragEnter={(e) => { handleDragEnter(e, task, index) }}
+                                onDragEnd={(e) => { handleDragEnd(e) }}
+                                className={`bg-success text-white m-1 p-3`}
+                                key={index}
+                                value={task.id}>
+                                {task.taskName}
+                            </animated.div>
+                        }
+
                         return <div
                             draggable='true'
                             onDragStart={(e) => { handleDragStart(e, task, index) }}
-                            onDragOver={handleDragOver}
+                            onDragEnter={(e) => { handleDragEnter(e, task, index) }}
                             onDragEnd={(e) => { handleDragEnd(e) }}
-                            className='bg-success text-white m-1 p-3'
+                            className={`bg-success text-white m-1 p-3 ${cssDragTag}`}
                             key={index}
                             value={task.id}>
                             {task.taskName}
@@ -53,14 +122,13 @@ export default function DemoDragDrop(props) {
                 <div
                     className='col-4 bg-primary'
                     style={{ height: 500 }}
-                    draggable='true'
-                    onDragOver={(e) => {
-                        e.stopPropagation();
-                        e.preventDefault();
-                    }}
-                    onDrop={(e) => { handleDrop(e) }}
                 >drop</div>
             </div>
         </div>
     )
 }
+
+// onDragOver={(e) => {
+//     e.stopPropagation();
+//     e.preventDefault();
+// }}
